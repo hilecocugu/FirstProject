3e47e0605ce5b6c271cb87c3ca5fa34e9f0c7c51
DESKTOP-IBEKMA8\PC <atici25yakup@gmail.com>
2024-10-06 11:32:12 +0300

    no message

---
+++ Projeler/Proje_1/Program.cs
@@ -0,0 +1,27 @@
+﻿int kalmasüresi;
+double ucret;
+Console.WriteLine("-----------Oto Park-----------");
+Console.Write("Kaç Dakika Duracaksın: ");
+kalmasüresi = Convert.ToInt32(Console.ReadLine());
+
+if (kalmasüresi <= 180)
+{
+    Console.WriteLine((kalmasüresi / 60) + "Saat " + (kalmasüresi % 60) + " Dakika kaldın " + "Ücret 7 Tl");
+}
+else if (kalmasüresi <= 420)
+{
+    ucret = 7 + ((kalmasüresi / 60 - 3) * 2);
+    Console.WriteLine((kalmasüresi / 60) + "Saat " + (kalmasüresi % 60) + " Dakika kaldın " + "Ücretin " + (ucret) + "Tl");
+}
+else if (kalmasüresi <= 720)
+{
+    ucret = 7 + (4 * 2) + (kalmasüresi / 60 - 7) * 1.5;
+    Console.WriteLine((kalmasüresi / 60) + "Saat " + (kalmasüresi % 60) + " Dakika kaldın " + "Ücretin " + (ucret) + "Tl");
+}
+else if (kalmasüresi > 720)
+{
+    ucret = 7 + (4 * 2) + (5 * 1.5) + (kalmasüresi / 60 - 12) * 1;
+    Console.WriteLine((kalmasüresi / 60) + "Saat " + (kalmasüresi % 60) + " Dakika kaldın " + "Ücretin " + (ucret) + "Tl");
+}
+
+Console.Read();
